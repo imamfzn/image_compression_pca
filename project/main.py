af420e4c3a84fd9058f2fe75f0cc963d6f7c9f8b
Imam Fauzan <imamfzn@gmail.com>
2017-06-15 15:51:05 +0700

    change mean

--- project/main.py
+++ project/main.py
@@ -5,7 +5,7 @@ import sys
 import math
 
 def pca(X, numpc):
-    mean = np.mean(X, axis = 0)
+    mean = np.mean(X)
     cov_mat = (X - mean).T.dot((X - mean)) / (X.shape[0] - 1)
     eig_vals, eig_vecs = np.linalg.eig(cov_mat)
 
